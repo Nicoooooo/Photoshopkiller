e91748fda4cb1af5470b8b6b347a4aee7af03e5e
NGauville <noreo.ercoli@gmail.com>
2016-01-14 15:34:08 +0100

    test readpgm/writepgm/interest

--- photoshopkiller/tests/InterestTest.java
+++ photoshopkiller/tests/InterestTest.java
@@ -5,13 +5,10 @@ import photoshopkiller.utils.ArrayUtils;
 
 public class InterestTest {
 	public InterestTest() {
-		int[][] tab = {
-			{3, 11, 24, 39},
-			{8, 21, 29, 39},
-			{200, 60, 25, 0}
-		};
+		int[][] tab = SeamCarving.readpgm("ex1.pgm");
 		
-		ArrayUtils.trace(tab);
-		ArrayUtils.trace(SeamCarving.interest(tab));
+		tab = SeamCarving.interest(tab);
+		
+		SeamCarving.writepgm(tab, "ex1_interest");
 	}
 }
